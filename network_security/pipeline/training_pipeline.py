9fd4e93a86022e66ceaa73a1e029a991f535d944
prashdraksh <prashdraksh@gmail.com>
2026-09-08 17:06:03 +0530

    completed total setups

--- network_security/pipeline/training_pipeline.py
+++ network_security/pipeline/training_pipeline.py
@@ -88,21 +88,21 @@ class TrainingPipeline:
             raise CustomException(e, sys)
 
     ## local artifact is going to s3 bucket    
-    def sync_artifact_dir_to_s3(self):
-        try:
-            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
-            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
-        except Exception as e:
-            raise CustomException(e,sys)
+    # def sync_artifact_dir_to_s3(self):
+    #     try:
+    #         aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+    #         self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
+    #     except Exception as e:
+    #         raise CustomException(e,sys)
         
-    ## local final model is going to s3 bucket 
+    # ## local final model is going to s3 bucket 
         
-    def sync_saved_model_dir_to_s3(self):
-        try:
-            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
-            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.model_dir,aws_bucket_url=aws_bucket_url)
-        except Exception as e:
-            raise CustomException(e,sys)
+    # def sync_saved_model_dir_to_s3(self):
+    #     try:
+    #         aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
+    #         self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.model_dir,aws_bucket_url=aws_bucket_url)
+    #     except Exception as e:
+    #         raise CustomException(e,sys)
         
     
     
